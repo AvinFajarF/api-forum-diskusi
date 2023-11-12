9efd1cb25480551e6b65a4aac3f90f98a4a3116f
AvinFajarF <avinfajar6@gmail.com>
2023-11-12 15:25:56 +0700

    chore: added migration

---
+++ database/migrations/2023_11_12_081922_create_saved_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('saved', function (Blueprint $table) {
+            $table->id();
+            // relasi ke table users
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+            // relasi ke table threds
+            $table->unsignedBigInteger('threads_id');
+
+            $table->foreign('threads_id')->references('id')->on('threads');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('saved');
+    }
+};
